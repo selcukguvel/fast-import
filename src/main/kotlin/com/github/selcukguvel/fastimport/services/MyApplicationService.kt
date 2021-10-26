fde917720a530f6f4fea2444e597fbb84bf97bbd
GitHub Action <action@github.com>
2021-10-26 21:18:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/selcukguvel/fastimport/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/selcukguvel/fastimport/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.selcukguvel.fastimport.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.selcukguvel.fastimport.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/selcukguvel/fastimport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/selcukguvel/fastimport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.selcukguvel.fastimport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.selcukguvel.fastimport.MyBundle
 
 class MyProjectService(project: Project) {
 
